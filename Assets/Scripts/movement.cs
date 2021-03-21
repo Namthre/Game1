8d632723871d2eb4b4b6059d968ecd9316565565
Namthre <namthre@gmail.com>
2021-03-21 15:21:03 -0400

    Initial Commit

---
+++ Assets/Scripts/movement.cs
@@ -0,0 +1,33 @@
+﻿//Alex Schroder
+//Program for dictating enemy movement
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public float startz;
+    public float starty;
+    public float startx;
+    public float endX;
+    public float endy;
+    public float endz;
+    public float speed;
+
+    void Update()
+    {
+        mover();
+    }
+
+    void mover()
+    {
+        this.transform.Translate(3f * Time.deltaTime * speed, 0f, 0f);
+
+        if (this.transform.position.x >= endX || this.transform.position.x < startx)
+        {
+            speed = speed * -1;
+        }
+
+
+    }
+}
